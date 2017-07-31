e2becf3c3847221c269097492717f5c536b1461e
Eric Iacutone <eric.iacutone@gmail.com>
2017-07-31 14:28:15 -0400

    Basic emacs config

---
+++ emacs.d/init.el
@@ -0,0 +1,26 @@
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+
+(use-package try
+    :ensure t)
+
+(use-package which-key
+    :ensure t 
+    :config
+    (which-key-mode))
+
+(require 'evil)
+(evil-mode 1)
+
+(require 'helm-config)
