9042915abb2c2f714142f4e2bde11b645e76c691
Eric Iacutone <eric.iacutone@gmail.com>
2017-08-15 18:08:29 -0400

    Add ace-window

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -33,3 +33,13 @@
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
 (helm-mode 1)
+(use-package ace-window
+  :ensure t
+  :init
+  (progn
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 3.0))))) 
+    ))
+(global-set-key (kbd "M-p") 'ace-window)
