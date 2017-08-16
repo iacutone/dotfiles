3f5b633beb41680ca8a5153982311f90dd9180f3
Eric Iacutone <eric.iacutone@gmail.com>
2017-08-16 19:36:21 -0400

    Pull emacs packages into org file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,76 +1,19 @@
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-
-(setq inhibit-startup-message t)
-(if window-system
-    (tool-bar-mode 0))
-(setq initial-scratch-message nil)
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
-
-(use-package try
-    :ensure t)
-
-(use-package which-key
-    :ensure t 
-    :config
-    (which-key-mode))
-
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
-(require 'evil)
-(evil-mode 1)
-
-(require 'helm-config)
-(global-set-key (kbd "M-x") #'helm-M-x)
-(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
-(helm-mode 1)
-
+(setq evil-want-C-i-jump nil)
 (defalias 'list-buffers 'ibuffer) ; make ibuffer default
 
-(use-package ace-window
-  :ensure t
-  :init
-  (progn
-    (global-set-key [remap other-window] 'ace-window)
-    (custom-set-faces
-     '(aw-leading-char-face
-       ((t (:inherit ace-jump-face-foreground :height 3.0))))) 
-    ))
-(global-set-key (kbd "M-p") 'ace-window)
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
-(use-package swiper
-  :ensure t
-  :bind
-  ("C-s" . swiper)
-  ("C-r" . swiper))
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
 
-(use-package avy
-  :ensure t
-  :bind ("M-s" . avy-goto-word-1))
-
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
-
-(use-package zenburn-theme
-  :ensure t
-  :config (load-theme 'zenburn t))
+(org-babel-load-file (expand-file-name "~/dotfiles/emacs.d/myinit.org"))
