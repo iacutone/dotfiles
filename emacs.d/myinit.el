ed28233cc598be0b0848c75e0c1e4e46bb3c7f2b
Eric Iacutone <eric.iacutone@gmail.com>
2020-10-21 15:45:44 -0400

    Add ivy and ledger-modee packages

--- emacs.d/myinit.el
+++ emacs.d/myinit.el
@@ -40,6 +40,30 @@
   :ensure t
   :bind ("M-s" . avy-goto-word-1))
 
+(use-package ivy)
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+;; enable this if you want `swiper' to use it
+;; (setq search-default-mode #'char-fold-to-regexp)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
 (use-package swiper
   :ensure t
   :bind
@@ -432,3 +456,11 @@
 	 ))
 
 (setq rtags-display-result-backend 'helm)
+
+(use-package ledger-mode
+  :ensure t
+  :defer t
+  :init
+  )
+
+(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
